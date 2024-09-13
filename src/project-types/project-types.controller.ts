8cce8b73839d0cbf8c80d7ce75ae958a45e8f594
Atia-Khan <atia.khan@xloopdigital.com>
2024-09-13 12:07:56 +0500

    fixes done

--- src/project-types/project-types.controller.ts
+++ src/project-types/project-types.controller.ts
@@ -1,37 +1,3 @@
-// import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-// import { ProjectTypesService } from './project-types.service';
-// import { CreateProjectTypeDto } from './dto/create-project-type.dto';
-// import { UpdateProjectTypeDto } from './dto/update-project-type.dto';
-
-// @Controller('project-types')
-// export class ProjectTypesController {
-//   constructor(private readonly projectTypesService: ProjectTypesService) {}
-
-//   @Post()
-//   create(@Body() createProjectTypeDto: CreateProjectTypeDto) {
-//     return this.projectTypesService.create(createProjectTypeDto);
-//   }
-
-//   @Get()
-//   findAll() {
-//     return this.projectTypesService.findAll();
-//   }
-
-//   @Get(':id')
-//   findOne(@Param('id') id: string) {
-//     return this.projectTypesService.findOne(+id);
-//   }
-
-//   @Patch(':id')
-//   update(@Param('id') id: string, @Body() updateProjectTypeDto: UpdateProjectTypeDto) {
-//     return this.projectTypesService.update(+id, updateProjectTypeDto);
-//   }
-
-//   @Delete(':id')
-//   remove(@Param('id') id: string) {
-//     return this.projectTypesService.remove(+id);
-//   }
-// }
 
 
 import { Controller, Get, Post, Param, Body, Put, Delete } from '@nestjs/common';
@@ -42,13 +8,13 @@ export class ProjectTypesController {
   constructor(private readonly projectTypesService: ProjectTypesService) {}
 
  
-  @Post('/add')
+  @Post()
   async create(@Body('name') name: string) {
     return this.projectTypesService.create(name);
   }
 
  
-  @Get('/get')
+  @Get()
   async findAll() {
     return this.projectTypesService.findAll();
   }
